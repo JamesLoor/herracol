21d0d6dffbeb1ba5eea8437ed1d8daeee0a2e55d
James Loor <mendozajames02@hotmail.com>
2022-02-18 08:36:43 -0500

    Fix HeaerAdmin visualization

--- src/components/HeaderAdmin.jsx
+++ src/components/HeaderAdmin.jsx
@@ -8,10 +8,9 @@ const HeaderAdminStyled = styled.div`
   width: 100%;
   position: fixed;
   align-items: center;
-  padding: 20px 2%;
+  padding: 20px 2% 0 2%;
   z-index: ${theme.positions.header};
-  background: #FFFFFF;
-
+  background: linear-gradient(180deg,hsla(0,0%,97.3%,.95) 44%,hsla(0,0%,97.3%,.46) 73%,hsla(0,0%,100%,0));
   .header__container {
     width: 100%;
     display: grid;
@@ -20,10 +19,9 @@ const HeaderAdminStyled = styled.div`
     justify-content: space-between;
     align-items: center;
     background-color: ${theme.colors.primary};
-    box-shadow: 0px 8px 24px rgba(0, 0, 0, 0.13);
     padding: 12px 20px;
     border-radius: 8px;
-    box-shadow: 0 4px 24px 0 rgb(34 41 47 / 10%);
+    box-shadow: 0px 6px 20px 6px rgb(0 0 0 / 10%);
   }
 
   @media (min-width: 1024px) {
