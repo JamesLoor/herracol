f4f354ec4f4c021d710be920ef7b518b9874b99d
James Loor <mendozajames02@hotmail.com>
2022-02-18 00:19:48 -0500

    Add mock data to test userList

---
+++ src/data/mock/userList.data.mock.js
@@ -0,0 +1,93 @@
+export const userList = [
+  {
+    "id": "8d0454c7-39df-43b9-992c-81c20768b03a",
+    "fullname": "Clementine MacElholm",
+    "email": "cmacelholm0@devhub.com",
+    "rol": "facturación",
+    "estado": "activo"
+  }, {
+    "id": "b12b5988-2022-41ce-a552-6328e89c1a18",
+    "fullname": "Wilhelmine Proudman",
+    "email": "wproudman1@php.net",
+    "rol": "admin",
+    "estado": "activo"
+  }, {
+    "id": "74943a02-c47d-4342-ba74-a1251b17b98d",
+    "fullname": "Ashien Barwell",
+    "email": "abarwell2@aboutads.info",
+    "rol": "admin",
+    "estado": "inactivo"
+  }, {
+    "id": "c709de37-73fb-4eea-a936-0ce0279879b9",
+    "fullname": "Cody Eby",
+    "email": "ceby3@themeforest.net",
+    "rol": "ventas",
+    "estado": "activo"
+  }, {
+    "id": "cf996a70-8469-4dd7-b3f7-531c6c5c7422",
+    "fullname": "Ring Wakerley",
+    "email": "rwakerley4@wikipedia.org",
+    "rol": "bodega",
+    "estado": "activo"
+  }, {
+    "id": "87a2f9b0-c95a-43b6-84ce-706d92ff6ece",
+    "fullname": "Nikolas Stairmand",
+    "email": "nstairmand5@barnesandnoble.com",
+    "rol": "facturación",
+    "estado": "activo"
+  }, {
+    "id": "afabf8ce-f2d7-4232-a67b-17c5241b55d6",
+    "fullname": "Dorie Margaret",
+    "email": "dmargaret6@woothemes.com",
+    "rol": "bodega",
+    "estado": "inactivo"
+  }, {
+    "id": "050ba0ba-939d-420d-af73-2a14b4b5cb34",
+    "fullname": "Adan Bloy",
+    "email": "abloy7@mozilla.com",
+    "rol": "bodega",
+    "estado": "activo"
+  }, {
+    "id": "59dd66d4-7e4c-414e-8286-e04f0c39c6ba",
+    "fullname": "Brunhilde Fenelow",
+    "email": "bfenelow8@privacy.gov.au",
+    "rol": "ventas",
+    "estado": "inactivo"
+  }, {
+    "id": "223dc0bf-5e36-48cc-84d3-524325a6bdc5",
+    "fullname": "Britteny Giacovetti",
+    "email": "bgiacovetti9@techcrunch.com",
+    "rol": "bodega",
+    "estado": "inactivo"
+  }, {
+    "id": "9efdd549-2daf-4898-b6f4-ad47601f8a62",
+    "fullname": "Jena Woolrich",
+    "email": "jwoolricha@photobucket.com",
+    "rol": "ventas",
+    "estado": "activo"
+  }, {
+    "id": "d26c9ce3-2da2-4942-b2e7-9042db97c32a",
+    "fullname": "Danette Wilce",
+    "email": "dwilceb@google.com",
+    "rol": "ventas",
+    "estado": "activo"
+  }, {
+    "id": "e34ba3a9-eae9-4407-858f-2f28807de78d",
+    "fullname": "Carolus Allen",
+    "email": "callenc@go.com",
+    "rol": "facturación",
+    "estado": "inactivo"
+  }, {
+    "id": "03d89e4a-c486-4b45-b943-225710668802",
+    "fullname": "Lilli Ortes",
+    "email": "lortesd@twitter.com",
+    "rol": "admin",
+    "estado": "inactivo"
+  }, {
+    "id": "76240caf-093c-4a40-a93d-af59271e76e9",
+    "fullname": "Ross McGilben",
+    "email": "rmcgilbene@spotify.com",
+    "rol": "facturación",
+    "estado": "activo"
+  }
+]
